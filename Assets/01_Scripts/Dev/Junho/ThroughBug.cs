837113677f5d13f422c83851dd0dc835862f2626
김준호 <kjhlove0119@naver.com>
2022-07-11 16:27:10 +0900

    아직 미완 // 불 값이 안바뀜

--- Assets/01_Scripts/Dev/Junho/ThroughBug.cs
+++ Assets/01_Scripts/Dev/Junho/ThroughBug.cs
@@ -8,44 +8,61 @@ public class ThroughBug : MonoBehaviour
     [SerializeField] private GameObject _playerR = null;
     [SerializeField] private GameObject _playerL = null;
     
-    [SerializeField] private float _bugTriggerTime = 0f;
+    [SerializeField] private float _bugTriggerTimeR = 0f;
+    [SerializeField] private float _bugTriggerTimeL = 0f;
 
-    private bool _isTriggerOn = false;
+    [SerializeField] private bool _isTriggerOnR = false;//시간 초 흐르게 하는 변수
+    [SerializeField] private bool _isTriggerOnL = false;//시간 초 흐르게 하는 변수
+    [SerializeField] private bool _isBugRunning = false;//버그 실행하게 하는 변수
 
     private void Awake()
     { 
+
     }
 
     private void Update()
     {
-        if(_isTriggerOn == true)
-        {
-            TimeCheck(); 
-        }
+        TimeCheck(); 
         ThroughBugRunning();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _isTriggerOn = true;
+        if(collision.gameObject == _playerR)
+        {
+            _isTriggerOnR = true;
+        }
+        if(collision.gameObject == _playerL)
+        {
+            _isTriggerOnL = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _bugTriggerTime = 0f;
-        _isTriggerOn = false;
+        _bugTriggerTimeR = 0f;
+        _bugTriggerTimeL = 0f;
+        _isTriggerOnR = false;
+        _isTriggerOnL = false;
     }
     
     private void TimeCheck()
     {
-        _bugTriggerTime += Time.deltaTime;
+        if(_isTriggerOnR == true)
+        {
+            _bugTriggerTimeR += Time.deltaTime;
+        }
+        if(_isTriggerOnL == true)
+        {
+            _bugTriggerTimeL += Time.deltaTime;
+        }
     }
 
     private void ThroughBugRunning()
     {
-        if (_bugTriggerTime > 3f)
+        if (_bugTriggerTimeR > 3f)
         {
-            
+            _playerR.transform.position -= new Vector3(0, 5, 0);
         }
     }
 }
